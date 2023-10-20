0aaee60ad2134b8ae72c9d13fa9cfb906d5d61d5
LuDev01 <luisatoro0121@hotmail.com>
2023-10-20 11:43:56 -0500

    Files ready to be used

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css'; // Imports the CSS file for styling the App component.
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom"; //Imports components and utilities from the react-router-dom library. Specifically, it imports BrowserRouter as Router, Routes, and Route. These components are used for setting up client-side routing in a React application.
+// import NavBar from './components/NavBar'; 
+import { HomePage } from './pages/HomePage';
+
+function App() { // Defines the App component, which serves as the main component for the React application.
+  return (
+    <Router> {/*Starts the router context provided by BrowserRouter. This allows you to use features like navigation and route matching.*/}
+      {/* <NavBar/> */}
+    <div className="App"> {/*Provide a container for the main content of the application.*/}
+    <Routes> {/*Sets up a container for defining different routes in the application.*/}
+      <Route path='/' element={<HomePage/>}/> {/*Defines a route for the root path / that renders the HomePage component when the root path is matched.*/}
+    </Routes>
+    </div>
+    </Router>
+  );
+}
+export default App; //xports the App component as the default export, making it available for use in other parts of the application.
+
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return (
+      <div className='container'>
+          <h1>Welcome to General Shop, your everyday's style!</h1>
+      </div>
+    )
+  }
+  
