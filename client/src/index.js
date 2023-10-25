9d9cb1f32d2343c49d5831c0555efcefff7b31b7
LuDev01 <luisatoro0121@hotmail.com>
2023-10-24 21:29:32 -0500

    Modifying the main file

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'; //  Imports the React library. React is a JavaScript 
 import ReactDOM from 'react-dom/client'; //Imports the ReactDOM module from the react-dom package. The .client part is used to import the client-side renderer.
 import App from './App'; //Imports the App component from the './App' file
 import 'bootstrap/dist/css/bootstrap.min.css'; //Imports the Bootstrap CSS file, providing styling for the application. Bootstrap is a popular CSS framework for building responsive and visually appealing web pages.
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Creates a root for the React application using ReactDOM.createRoot. This root is associated with an HTML element with the ID 'root', typically the container where the React app will be rendered.
 root.render(
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import {Home} from "../components/Home"
+import { LoginForm } from "../components/LoginForm"
+import { NavBar } from "../components/NavBar"
 
 export const HomePage = () => {
   return (
     <>
+    <NavBar/>
     <Home/>
+    <LoginForm/>
     </>
   )
   
