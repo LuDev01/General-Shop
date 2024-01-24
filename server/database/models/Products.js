d22778fc58ae4919846882907bfd83f527754ef0
LuDev01 <luisatoro0121@hotmail.com>
2024-01-24 11:35:48 -0500

    Sizes reconfiguration
    Modifying the schema, controller and components to set the sizes and quantity as a complete object

--- server/database/models/Products.js
+++ server/database/models/Products.js
@@ -1,54 +1,55 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const productsSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const productsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+
+  sizes: {
+    XS: { type: Number, default: 0 },
+    S: { type: Number, default: 0 },
+    M: { type: Number, default: 0 },
+    L: { type: Number, default: 0 },
+    XL: { type: Number, default: 0 },
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    public_id: {
+      type: String,
+      required: true,
     },
-    category:{
-        type:String,
-        required:true
+    url: {
+      type: String,
+      required: true,
     },
-    brand:{
-        type:String,
-        required:true
-    },
-    color:{
-        type:String,
-        required:true
-    },
-    size:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-    quantity:{
-        type:Number,
-        required:true
-    },
-    description:{
-        type:String,
-        required:true
-    },
-    image:{
-        public_id: {
-            type: String,
-            required: true
-        },
-        url: {
-            type: String,
-            required: true
-        }
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'userRole',
-        ref: 'Users'
-    }
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "userRole",
+    ref: "Users",
+  },
 });
 
-const Products=mongoose.model('Products',productsSchema);
-module.exports=Products;
+const Products = mongoose.model("Products", productsSchema);
+module.exports = Products;
