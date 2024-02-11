3af01fa1ae6879af44c1cdb93a26573795a36be9
Mariajose Gomez <mariajgomezg1@gmail.com>
2024-02-11 11:49:32 -0500

    Working in the responsive
    Responsive: Navbar & home page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext, useEffect } from "react";
-import { Link, useNavigate,useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ImSearch } from "react-icons/im";
 import { FaUserAlt } from "react-icons/fa";
 import { ImageContext } from "./context/ImageContext";
@@ -20,6 +20,7 @@ export const NavBar = () => {
   const [value, setValue] = useState("");
   const [userDropdown, setUserDropdown] = useState(false);
   const [data, setData] = useState([]);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
   const navigate = useNavigate();
 
   const token = localStorage.getItem("token");
@@ -42,16 +43,6 @@ export const NavBar = () => {
     setIsSearchVisible(!isSearchVisible);
     setIsDropdownVisible(!isDropdownVisible);
   };
-
-  // const changeBackground = () => {
-  //   if (window.scrollY >= 80) {
-  //     setNavbar(true);
-  //   } else {
-  //     setNavbar(false);
-  //   }
-  // };
-
-  // window.addEventListener("scroll", changeBackground);
 
   const handleOnChangeValue = (e) => {
     setValue(e.target.value);
@@ -82,26 +73,39 @@ export const NavBar = () => {
     window.location.reload();
   };
 
-
   const getProduct = async () => {
     try {
       const response = await axiosClient.get(`products?search=${value}`);
       setData(response.data.products);
-      console.log("here the data",data);
+      console.log("here the data", data);
     } catch (error) {
       console.error("Error showing product", error.message);
     }
   };
-useEffect(()=>{
-  getProduct();
-},[value])
+
+  useEffect(() => {
+    getProduct();
+  }, [value]);
+
+  const handleMenuToggle = () => {
+    setIsMenuVisible(!isMenuVisible);
+    setIsSearchVisible(false); // Oculta la barra de búsqueda al abrir el menú
+  };
+
+  const handleSearchToggle = () => {
+    setIsSearchVisible(!isSearchVisible);
+    setIsDropdownVisible(!isDropdownVisible);
+    setIsMenuVisible(false); // Oculta el menú al abrir la barra de búsqueda
+  };
 
   return (
     <div>
       <Navbar
         expand="lg"
         className={
-          navbar ? "NavBar-color active fixed-top" : "NavBar-color fixed-top"
+          navbar
+            ? "NavBar-color active fixed-top"
+            : "NavBar-color fixed-top"
         }
       >
         <Container>
@@ -110,9 +114,12 @@ useEffect(()=>{
               <img className="general-logo" src={logo} alt="" />
             </Navbar.Brand>
           </div>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            onClick={handleMenuToggle}
+          />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav style={{ position: "relative", right: 90 }}>
+            <Nav className={`me-auto ${isMenuVisible ? "mobile-menu" : ""}`}>
               <Nav.Link href="/" className="nav-menu-text">
                 Home
               </Nav.Link>
@@ -123,18 +130,20 @@ useEffect(()=>{
               >
                 About Us
               </Nav.Link>
-              {/* <NavDropdown  title="Categories" id="basic-nav-dropdown"  > */}
               <NavDropdown
                 title={<span className="nav-menu-text">Categories</span>}
                 id="basic-nav-dropdown"
               >
-                <NavDropdown.Item href="/womenProducts">Woman</NavDropdown.Item>
+                <NavDropdown.Item href="/womenProducts">
+                  Woman
+                </NavDropdown.Item>
                 <NavDropdown.Item href="/menProducts">Man</NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Form
-              className="d-flex search-field search-container"
-              style={{ opacity: search.opacity, transition: search.transition }}
+              className={`d-flex search-field search-container ${
+                isSearchVisible ? "mobile-search" : ""
+              }`}
             >
               <Form.Control
                 type="search"
@@ -144,47 +153,52 @@ useEffect(()=>{
                 value={value}
                 onChange={handleOnChangeValue}
               />
-            {value && data && (
-              <div className="dropdown-search">
-                {data
-                  .filter((product) => {
-                    const searchItems = value.toLowerCase().split(" ");
-                    return searchItems.every(
-                      (item) =>
-                      product.name.toLowerCase().includes(item) ||
-                      product.color.toLowerCase().includes(item) ||
-                      product.category.toLowerCase().includes(item)
+              {value && data && (
+                <div className="dropdown-search">
+                  {data
+                    .filter((product) => {
+                      const searchItems = value.toLowerCase().split(" ");
+                      return searchItems.every(
+                        (item) =>
+                          product.name.toLowerCase().includes(item) ||
+                          product.color.toLowerCase().includes(item) ||
+                          product.category.toLowerCase().includes(item)
                       );
                     })
-                  .slice(0, 3)
-
-                  .map((product) => (
-                    <Link
-                    className="dropdown-search-row"
-                    to={`/productDetails/${product._id}`}
-                    onClick={() => handleOnSearch(product.name)}
-                    key={product._id}
-                    style={{ textDecoration: "none", color: "black" }}
-                    >
-                      {product.name}
-                      <img
-                        className="dropdown-search-img"
-                        src={product.image.url}
-                        alt="products"
+                    .slice(0, 3)
+                    .map((product) => (
+                      <Link
+                        className="dropdown-search-row"
+                        to={`/productDetails/${product._id}`}
+                        onClick={() => handleOnSearch(product.name)}
+                        key={product._id}
+                        style={{
+                          textDecoration: "none",
+                          color: "black",
+                        }}
+                      >
+                        {product.name}
+                        <img
+                          className="dropdown-search-img"
+                          src={product.image.url}
+                          alt="products"
                         />
-                    </Link>
-                  ))}
-              </div>
-                  )}
+                      </Link>
+                    ))}
+                </div>
+              )}
             </Form>
 
-            {search.showSearchButton ? (
-              <ImSearch className="search-icon" onClick={showIcon} />
-            ) : null}
+            {!isSearchVisible && (
+              <ImSearch
+                className="search-icon"
+                onClick={handleSearchToggle}
+              />
+            )}
 
             {isLoggedIn ? (
               role === "Client" ? (
-                <Nav style={{ position: "relative", right: 17 }}>
+                <Nav className={`user-dropdown ${isMenuVisible ? "mobile-menu" : ""}`}>
                   <img
                     src={imageURLClient}
                     style={{
@@ -192,7 +206,7 @@ useEffect(()=>{
                       width: "32px",
                       height: "32px",
                     }}
-                    alt="profle-picture"
+                    alt="profile-picture"
                     onClick={() => setUserDropdown(!userDropdown)}
                   />
                   <NavDropdown show={userDropdown} className="user-dropdown">
@@ -205,7 +219,7 @@ useEffect(()=>{
                   </NavDropdown>
                 </Nav>
               ) : role === "Admin" ? (
-                <Nav style={{ position: "relative", right: 17 }}>
+                <Nav className={`user-dropdown ${isMenuVisible ? "mobile-menu" : ""}`}>
                   <img
                     src={imageURL}
                     style={{
@@ -213,7 +227,7 @@ useEffect(()=>{
                       width: "3rem",
                       height: "3rem",
                     }}
-                    alt="profle-picture"
+                    alt="profile-picture"
                     onClick={() => setUserDropdown(!userDropdown)}
                   />
                   <NavDropdown show={userDropdown} className="user-dropdown">
@@ -242,3 +256,6 @@ useEffect(()=>{
     </div>
   );
 };
+
+
+
