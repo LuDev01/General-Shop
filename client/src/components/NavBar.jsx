cfaddba60d5327a56ca958fb641c10435cc57c18
Mariajose Gomez <mariajgomezg1@gmail.com>
2024-02-29 21:42:09 -0500

    Navbar new design

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { ImSearch, ImCross } from "react-icons/im";
 import { FaUserAlt } from "react-icons/fa";
 import { ImageContext } from "./context/ImageContext";
-import { DataContext } from "./context/DataContext";
 import CartModal from "./CartModal";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -30,7 +29,6 @@ export const NavBar = () => {
 
   const isLoggedIn = Boolean(token);
   const { imageURL, imageURLClient } = useContext(ImageContext);
-  const { setRole, setCart } = useContext(DataContext);
 
   const [search, setSearchBtn] = useState({
     transition: "all .3s ease",
@@ -51,29 +49,21 @@ export const NavBar = () => {
     setValue(e.target.value);
   };
 
-  const handleOnSearch = (searchItem, data) => {
+  const handleOnSearch = (searchItem,data) => {
     setValue(searchItem);
-    navigate({ to: `/productDetails/${data._id}` });
+    // <Link to={`/productDetails/${data._id}`}></Link>
+    navigate({to: `/productDetails/${data._id}`})
     console.log("searching", searchItem);
   };
   
+
   const handleOnLogOut = () => {
-    setRole("Guest");
     localStorage.setItem("isLoggedOut", "true");
     localStorage.removeItem("isLoggedIn");
     localStorage.removeItem("isLoggedInv2");
     localStorage.removeItem("token");
     localStorage.removeItem("exp");
     localStorage.removeItem("role");
-    localStorage.removeItem("user");
-
-    const savedCart = localStorage.getItem("myCart_Guest");
-    if (savedCart) {
-      setCart(JSON.parse(savedCart));
-    } else {
-      setCart([]);
-    }
-
     console.log("the super cookie", document.cookie);
     var cookies = document.cookie.split(";");
 
@@ -108,119 +98,84 @@ export const NavBar = () => {
   }, [value]);
 
   return (
-    <div>
-      <Navbar
-        expand="lg"
-        className={
-          navbar
-            ? "NavBar-color active fixed-top"
-            : "NavBar-color fixed-top"
-        }
-      >
-        <Container>
-          <div className="logo-container">
-            <Navbar.Brand href="/">
-              <img className="general-logo" src={logo} alt="" />
-            </Navbar.Brand>
-          </div>
-          {/* <Navbar.Toggle
-            aria-controls="basic-navbar-nav"
-            onClick={handleMenuToggle}
-          /> */}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            {/* <Nav className={`me-auto ${isMenuVisible ? "mobile-menu" : ""}`}> */}
-            <Nav style={{ position: "relative", right: 90 }}>
-              <Nav.Link href="/" className="nav-menu-text">
-                Home
-              </Nav.Link>
-              <Nav.Link
-                className="nav-menu-text"
-                style={{ whiteSpace: "nowrap" }}
-                href="/aboutUs"
-              >
-                About Us
-              </Nav.Link>
-              <NavDropdown
-                title={<span className="nav-menu-text">Categories</span>}
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item href="/womenProducts">
-                  Woman 
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/menProducts">Man</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            {/* <Form
-              className={`d-flex search-field search-container ${
-                isSearchVisible ? "mobile-search" : ""
-              }`}
-            > */}
-             <Form
-              className="d-flex search-field search-container"
-              style={{ opacity: search.opacity, transition: search.transition }}
-            >
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-                value={value}
-                onChange={handleOnChangeValue}
-              />
-              {value && data && (
-                <div className="dropdown-search">
-                  {data
-                    .filter((product) => {
-                      const searchItems = value.toLowerCase().split(" ");
-                      return searchItems.every(
-                        (item) =>
-                          product.name.toLowerCase().includes(item) ||
-                          product.color.toLowerCase().includes(item) ||
-                          product.category.toLowerCase().includes(item)
-                      );
-                    })
-                    .slice(0, 3)
-                    .map((product) => (
-                      <Link
-                        className="dropdown-search-row"
-                        to={`/productDetails/${product._id}`}
-                        onClick={() => handleOnSearch(product.name)}
-                        key={product._id}
-                        style={{
-                          textDecoration: "none",
-                          color: "black",
-                        }}
-                      >
-                        {product.name}
-                        <img
-                          className="dropdown-search-img"
-                          src={product.image.url}
-                          alt="products"
-                        />
-                      </Link>
-                    ))}
-                </div>
-              )}
-            </Form>
-{/* 
-            {!isSearchVisible && (
-              <ImSearch
-                className="search-icon"
-                onClick={handleSearchToggle}
-              />
-            )} */}
-                 {search.showSearchButton ? (
+    <header>
+      <div className="group">
+      <div className="logo-container">
+        <Link to="/">
+          <img className="general-logo" src={logo} alt="" />
+        </Link>
+      </div>
+      
+      <div className="navigation-tabs">
+        {/* <ul className={`navigation ${isMenuVisible ? 'open' : ''}`}> */}
+        <ul className="navigation">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/aboutUs">About Us</a>
+          </li>
+          <li>
+            <a href="/womenProducts">Woman</a>
+          </li>
+          <li>
+            <a href="/menProducts">Man</a>
+          </li>
+        </ul>
+      </div>        
+        
+        <div>
+          {isSearchVisible ? (
+            <div className={`searchBox ${isSearchVisible ? 'active' : ''}`}>
+              <ImSearch className="search-product-icon" onClick={handleOnSearch} />
+              <input type="text" placeholder="S E A R C H   H E R E . . . " />
+              <ImCross className="close-icon" onClick={showIcon} />
+            </div>
+            ) : (
               <ImSearch className="search-icon" onClick={showIcon} />
-            ) : null}
-
-            {isLoggedIn ? (
-              role === "Client" ? (
-                <Nav
-                  className={`user-dropdown ${
-                    isMenuVisible ? "mobile-menu" : ""
-                  }`}
+            )}
+        </div>
+        {value && data && (
+          <div className="dropdown-search">
+            {data
+              .filter((product) => {
+                const searchItems = value.toLowerCase().split(" ");
+                return searchItems.every(
+                  (item) =>
+                    product.name.toLowerCase().includes(item) ||
+                    product.color.toLowerCase().includes(item) ||
+                    product.category.toLowerCase().includes(item)
+                );
+              })
+              .slice(0, 3)
+              .map((product) => (
+                <Link
+                  className="dropdown-search-row"
+                  to={`/productDetails/${product._id}`}
+                  onClick={() => handleOnSearch(product.name)}
+                  key={product._id}
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                  }}
                 >
+                  {product.name}
+                  <img
+                    className="dropdown-search-img"
+                    src={product.image.url}
+                    alt="products"
+                  />
+                </Link>
+              ))}
+          </div>
+        )}
+        {/* {search.showSearchButton ? (
+          <ImSearch className="search-icon" onClick={showIcon} />
+        ) : null} */}
+        <div>
+        {isLoggedIn ? (
+              role === "Client" ? (
+                <Nav className={`user-dropdown ${isMenuVisible ? "mobile-menu" : ""}`}>
                   <img
                     src={imageURLClient}
                     style={{
@@ -241,11 +196,7 @@ export const NavBar = () => {
                   </NavDropdown>
                 </Nav>
               ) : role === "Admin" ? (
-                <Nav
-                  className={`user-dropdown ${
-                    isMenuVisible ? "mobile-menu" : ""
-                  }`}
-                >
+                <Nav className={`user-dropdown ${isMenuVisible ? "mobile-menu" : ""}`}>
                   <img
                     src={imageURL}
                     style={{
@@ -280,4 +231,3 @@ export const NavBar = () => {
     </header>
   );
 };
-
